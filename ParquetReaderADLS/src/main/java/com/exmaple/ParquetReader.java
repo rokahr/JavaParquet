6c0697f5096fae944e2c32fe8416074aac7004c7
Roman Kahr <roman.kahr@me.com>
2020-07-24 13:20:03 +0200

    Add files via upload

--- ParquetReaderADLS/src/main/java/com/exmaple/ParquetReader.java
+++ ParquetReaderADLS/src/main/java/com/exmaple/ParquetReader.java
@@ -1,5 +1,5 @@
 package com.exmaple;
-import java.io.IOException;
+import java.io.*;
 import java.sql.SQLOutput;
 
 import org.apache.hadoop.conf.Configuration;
@@ -15,10 +15,24 @@ import org.apache.parquet.io.MessageColumnIO;
 import org.apache.parquet.io.RecordReader;
 import org.apache.parquet.schema.MessageType;
 import org.apache.parquet.schema.Type;
+import com.azure.storage.common.StorageSharedKeyCredential;
+import com.azure.storage.file.datalake.DataLakeDirectoryClient;
+import com.azure.storage.file.datalake.DataLakeFileClient;
+import com.azure.storage.file.datalake.DataLakeFileSystemClient;
+import com.azure.storage.file.datalake.DataLakeServiceClient;
+import com.azure.storage.file.datalake.DataLakeServiceClientBuilder;
+
+
 
 public class ParquetReader {
 
-    private static Path path = new Path("file:\\C:\\Users\\roman\\Documents\\ML\\Parquet\\example.parquet");
+    public static String storageAccount="yourstoragename";
+    public static String accountKey = "yourkey";
+    public static String blobName="yourblobname";
+    public static String folder = "yourfoldername";
+    public static String fileName="filename.parquet";
+
+    private static Path path = new Path("file:\\C:\\Users\\roman\\Downloads\\Parquet\\"+fileName);
 
     private static void printGroup(Group g, int r) {
         int fieldCount = g.getType().getFieldCount();
@@ -54,10 +68,50 @@ public class ParquetReader {
         System.out.println("");
     }
 
-    public static void main(String[] args) throws IllegalArgumentException {
+    static public DataLakeServiceClient GetDataLakeServiceClient
+            (String accountName, String accountKey){
+
+        StorageSharedKeyCredential sharedKeyCredential =
+                new StorageSharedKeyCredential(accountName, accountKey);
+
+        DataLakeServiceClientBuilder builder = new DataLakeServiceClientBuilder();
+
+        builder.credential(sharedKeyCredential);
+        builder.endpoint("https://" + accountName + ".dfs.core.windows.net");
+
+        return builder.buildClient();
+    }
+
+    static public void DownloadFile(DataLakeFileSystemClient fileSystemClient)
+            throws FileNotFoundException, java.io.IOException{
+
+        DataLakeDirectoryClient directoryClient =
+                fileSystemClient.getDirectoryClient(folder);
+
+        DataLakeFileClient fileClient =
+                directoryClient.getFileClient(fileName);
+
+        File file = new File("C:\\Users\\roman\\Downloads\\Parquet\\"+fileName);
+
+        OutputStream targetStream = new FileOutputStream(file);
+
+        fileClient.read(targetStream);
+
+        targetStream.close();
+
+    }
+
+    public static void main(String[] args) throws IllegalArgumentException, IOException {
+
+
 
         Configuration conf = new Configuration();
 
+        DataLakeServiceClient adlsClient = GetDataLakeServiceClient(storageAccount,accountKey);
+
+        DataLakeFileSystemClient dataLakeFileSystemClient = adlsClient.getFileSystemClient(blobName);
+
+        DownloadFile(dataLakeFileSystemClient);
         try {
             ParquetMetadata readFooter = ParquetFileReader.readFooter(conf, path, ParquetMetadataConverter.NO_FILTER);
             MessageType schema = readFooter.getFileMetaData().getSchema();
@@ -85,6 +139,21 @@ public class ParquetReader {
             System.out.println("Error reading parquet file.");
             e.printStackTrace();
         }
+        System.out.println();
+        System.out.println("To load the file directly into DataBricks please copy and paste the following code snippet into your DataBricks notebook:");
+        System.out.println("data = spark.read.parquet(\"abfss://"+blobName+"@"+storageAccount+".dfs.core.windows.net/"+folder+"/"+fileName+"\")");
+        System.out.println("data.show()");
+        System.out.println();
+        System.out.println("To see all files within this folder please use copy and paste the following code snippet into your DataBricks notebook");
+        System.out.println("configs = {\n" +
+                "  \"fs.azure.account.auth.type\": \"CustomAccessToken\",\n" +
+                "  \"fs.azure.account.custom.token.provider.class\":   spark.conf.get(\"spark.databricks.passthrough.adls.gen2.tokenProviderClassName\")\n" +
+                "}");
+        System.out.println("dbutils.fs.mount(\n" +
+                "  source = \"abfss://"+blobName+"@"+storageAccount+".dfs.core.windows.net\",\n" +
+                "  mount_point = \"/mnt/data\",\n" +
+                "  extra_configs = configs)");
+        System.out.println("dbutils.fs.ls(\"/mnt/data\") ");
     }
 }
 
